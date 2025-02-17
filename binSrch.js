22551c0cdc0f599253210acc6d94b93ee4e382eb
AlleyAndRogue <alejandro15@att.net>
2025-02-16 21:20:08 -0800

    Update binSrch.js
    comments

--- binSrch.js
+++ binSrch.js
@@ -1,7 +1,9 @@
+//binary search algorithm leet code practice.
+
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 let left =0;
 let right = arr.length;
-
+// we are looking for value;
 let target = 4;
 
 while(left<=right){
