6192c51a47ac6f921f864398280e71f3ec06a702
Alejandro Ortega <alejandroortega9000@gmail.com>
2025-02-16 18:52:57 -0800

    unique ele in arr

---
+++ uniqueNum.js
@@ -0,0 +1,18 @@
+let nums =[1,1,1,2,3,3,3,3,4,4,5];
+
+let k = 1;
+
+for(let i=1;i<nums.length;i++){
+    if(!(nums[i]===nums[i-1])){
+        nums[k]=nums[i];
+        k++;
+
+
+    }
+
+}
+for(let i=k;i<nums.length;i++){
+    nums[i]="_";
+}
+console.log("unique elements: "+k);
+console.log(nums); 
