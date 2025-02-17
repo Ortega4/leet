c96960a14bb894c0b5979693dea5d9f48f49b3f2
Alejandro Ortega <alejandroortega9000@gmail.com>
2025-02-16 19:09:34 -0800

    binarySrch

---
+++ binSrch.js
@@ -0,0 +1,25 @@
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let left =0;
+let right = arr.length;
+
+let target = 4;
+
+while(left<=right){
+
+    let middle = Math.floor(left+ (right-left)/2);
+    console.log("current middle: "+ middle);
+
+    if(target===arr[middle]){
+        console.log("target found at index: "+ middle);
+        break;
+    }
+    else if(target>arr[middle]){
+        left = middle+1;
+        console.log("left side trim, new left: "+ left);
+
+    }
+    else if(target<arr[middle]){
+        right = middle-1;
+        console.log("right side trim, new right: "+ right);
+    }
+}
