6d4b07f4773c2e63a5e858952f75042c220b68b1
Alejandro Ortega <alejandroortega9000@gmail.com>
2025-02-24 16:26:49 -0800

    update

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,7 +1,7 @@
 let nums = [1,3,5,6];
 
 let index = 0;
-let target = 1;
+let target = 7;
 
 let left = 0;
 let right = nums.length-1;
@@ -20,6 +20,38 @@ while(left<=right){
     else if(target<nums[middle]){
         right = middle-1;
     }
-   index = middle
+    
 }
-console.log(index);
+if(left>right){
+    index= left;
+}
+//console.log(index);
+
+
+//function method 
+var searchInsert = function(nums,target){
+
+  
+let left = 0;
+let right = nums.length-1;
+
+
+while(left<=right){
+    let middle = Math.floor(left+ (right-left)/2);
+    if(nums[middle]===target){
+        return middle;
+       
+        
+    }
+    else if(target>nums[middle]){
+        left = middle +  1;
+    }
+    else if(target<nums[middle]){
+        right = middle-1;
+    }
+    
+}
+    return left;
+}
+
+console.log(searchInsert(nums,4));
